5f476fba3e9b04dc379efd96f98b3118668fb402
adamhitzger <99116869+adamhitzger@users.noreply.github.com>
2023-10-05 16:30:47 +0200

    'commit'

--- apartments/sanity.config.ts
+++ apartments/sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   projectId: 'xc3et9jd',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool()],
 
   schema: {
     types: schemaTypes,
