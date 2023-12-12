286398db3ff4cfbe9f18bc9ecfe9e7e4bcabbf5e
adam.hitzger <adam.hitzger@icloud.com>
2023-12-12 16:51:55 +0100

    commit

--- app/en/component/Contact.tsx
+++ app/en/component/Contact.tsx
@@ -81,8 +81,8 @@ export default function Contact(){
      setIsLoading(true);
 
      emailjs.send(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+      "service_0mmwe12",
+      "template_euxzwe6",
         {
           from_name: form.fullname,
           to_name: "Jiří",
