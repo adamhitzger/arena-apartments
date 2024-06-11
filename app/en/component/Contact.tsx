10934c99642bf909ca83f6dc4a410ad5665bbdd6
adam.hitzger <adam.hitzger@icloud.com>
2024-06-11 21:10:36 +0200

    emaijs conf

--- app/en/component/Contact.tsx
+++ app/en/component/Contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 import * as THREE from 'three';
-import React, {useEffect, useState,  Suspense, useRef, useLayoutEffect, useMemo, MouseEvent}  from 'react';
-import {motion} from "framer-motion";
+import React, { useEffect, useState, Suspense, useRef, useLayoutEffect, useMemo, MouseEvent } from 'react';
+import { motion } from "framer-motion";
 import { Canvas, ThreeEvent, useLoader } from '@react-three/fiber';
 import { SVGLoader } from 'three-stdlib';
 import { MapControls, Shape, Sphere } from '@react-three/drei';
@@ -14,7 +14,7 @@ const hoveredCursor =
 const defaultCursor =
   'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBjbGlwLXBhdGg9InVybCgjY2xpcDApIj48Y2lyY2xlIGN4PSIzMiIgY3k9IjMyIiByPSIyNi41IiBzdHJva2U9ImJsYWNrIi8+PHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0zMiAzMkw0MS4xOTI0IDQxLjE5MjRMNDEuODk5NSA0MC40ODUzTDMyLjcwNzEgMzEuMjkyOUw0MS4xOTI0IDIyLjgwNzZMNDAuNDg1MyAyMi4xMDA1TDMyIDMwLjU4NThMMjMuNTE0NyAyMi4xMDA1TDIyLjgwNzYgMjIuODA3NkwzMS4yOTI5IDMxLjI5MjlMMjIuMTAwNSA0MC40ODUzTDIyLjgwNzYgNDEuMTkyNEwzMiAzMloiIGZpbGw9ImJsYWNrIi8+PHBhdGggZD0iTTUuMzY3MTEgMTIuNzM3M0wyLjY2OTQyIDIuNjY5NDJMMTIuNzM3MyA1LjM2NzExTDUuMzY3MTEgMTIuNzM3M1oiIHN0cm9rZT0iYmxhY2siLz48L2c+PGRlZnM+PGNsaXBQYXRoIGlkPSJjbGlwMCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSJ3aGl0ZSIvPjwvY2xpcFBhdGg+PC9kZWZzPjwvc3ZnPg=='
 
-function Cell({ color, shape, fillOpacity } : {color: THREE.Color, shape: THREE.Shape, fillOpacity: any}) {
+function Cell({ color, shape, fillOpacity }: { color: THREE.Color, shape: THREE.Shape, fillOpacity: any }) {
   const [hovered, hover] = useState(false)
   useEffect(() => void (document.body.style.cursor = hovered ? `url('${hoveredCursor}'), pointer` : `url('${defaultCursor}'), auto`), [
     hovered
@@ -27,13 +27,14 @@ function Cell({ color, shape, fillOpacity } : {color: THREE.Color, shape: THREE.
   )
 }
 
-function Svg({ url } : { url: string }) {
+function Svg({ url }: { url: string }) {
   const { paths } = useLoader(SVGLoader, url)
   const shapes = useMemo(
-    () => paths.flatMap((p) => p.toShapes(true).map((shape) => ({ 
-      shape, 
-      color: p.color, 
-      fillOpacity: p?.userData?.style?.fillOpacity }))),
+    () => paths.flatMap((p) => p.toShapes(true).map((shape) => ({
+      shape,
+      color: p.color,
+      fillOpacity: p?.userData?.style?.fillOpacity
+    }))),
     [paths]
   )
 
@@ -52,7 +53,7 @@ function Svg({ url } : { url: string }) {
   )
 }
 
-export default function Contact(){
+export default function Contact() {
   const formRef = useRef(null);
 
   const [form, setForm] = useState({
@@ -66,11 +67,11 @@ export default function Contact(){
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
-    setForm({...form, [name]: value})
+    setForm({ ...form, [name]: value })
   };
 
   const showToast = (
-   company: string, adress: string, phone: string
+    company: string, adress: string, phone: string
   ) => {
     toast.info(`Name: ${company}, Adress: ${adress}, Phone: ${phone}.`, {
       position: toast.POSITION.TOP_CENTER,
@@ -78,21 +79,21 @@ export default function Contact(){
   };
 
   const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
-     e.preventDefault();
-     setIsLoading(true);
+    e.preventDefault();
+    setIsLoading(true);
 
-     emailjs.send(
+    emailjs.send(
       process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_SERVICE_ID!,
       process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_TEMPLATE_ID!,
-        {
-          from_name: form.fullname,
-          to_name: "Jiří",
-          from_email: form.email,
-          to_email: "arena@arenaapartmentshb.cz",
-          message: form.phone + "\n" + form.msg
-        },
-        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
-     ).then(() => {
+      {
+        from_name: form.fullname,
+        to_name: "Jiří",
+        from_email: form.email,
+        to_email: "arena@arenaapartmentshb.cz",
+        message: form.phone + "\n" + form.msg
+      },
+      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+    ).then(() => {
       setIsLoading(false);
       setForm({
         fullname: "",
@@ -100,109 +101,109 @@ export default function Contact(){
         phone: "",
         msg: "",
       });
-     }).catch((error) => {
-        setIsLoading(false);
-        console.log(error);
-     });
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+    });
   };
 
-    return(
-      
-        <motion.div className="p-5 bg-black w-full h-full " id="contact"
-        initial={{opacity: 0}}
-        whileInView={{opacity: 1}}
-        >
-         <div className="w-full h-full" >
-         <div className=" max-w-fit mx-5 my-8 2xl:mx-8 ">
-            <ul className="list-disc">
-              <li className="text-white underline underline-offset-4 text-2xl 2xl:text-4xl">Contact</li>
-            </ul>
+  return (
+
+    <motion.div className="p-5 bg-black w-full h-full " id="contact"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+    >
+      <div className="w-full h-full" >
+        <div className=" max-w-fit mx-5 my-8 2xl:mx-8 ">
+          <ul className="list-disc">
+            <li className="text-white underline underline-offset-4 text-2xl 2xl:text-4xl">Contact</li>
+          </ul>
         </div>
-            <div className="w-full max-h-fit lg:flex">
-              <div className=" w-full my-3  p-5 border rounded-[25px] bg-white lg:w-1/2 lg:p-10 lg:ml-3  ">
-                <form className="2xl:m-5"  onSubmit={handleSubmit} ref={formRef}>
-                <div className="text-2xl my-5">
-                  <label htmlFor="fullname">Fullname</label>
+        <div className="w-full max-h-fit lg:flex">
+          <div className=" w-full my-3  p-5 border rounded-[25px] bg-white lg:w-1/2 lg:p-10 lg:ml-3  ">
+            <form className="2xl:m-5" onSubmit={handleSubmit} ref={formRef}>
+              <div className="text-2xl my-5">
+                <label htmlFor="fullname">Fullname</label>
+              </div>
+              <div className="mb-5 border-b-2 border-solid border-black">
+                <input className=" w-full p-2" type="text" name='fullname' id='fullname' value={form.fullname} onChange={handleChange} required />
+              </div>
+              <div className="text-2xl my-5">
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="mb-5 border-b-2 border-solid border-black">
+                <input className="w-full p-2" type="email" name="email" id='email' value={form.email} onChange={handleChange} required />
+              </div>
+              <div className="text-2xl my-5">
+                <label htmlFor="phone">Phone</label>
+              </div>
+              <div className="mb-5 border-b-2 border-solid border-black">
+                <input className=" w-full p-2" type="text" name='phone' id='phone' value={form.phone} onChange={handleChange} required />
+              </div>
+              <div className="text-2xl my-5">
+                <label htmlFor="msg">Message</label>
+              </div>
+              <div className="mb-5 border-b-2 border-solid border-black">
+                <textarea className=" w-full p-2" name='msg' id='msg' value={form.msg} onChange={handleChange} required></textarea>
+              </div>
+              <div className="text-xl font-semibold ">
+                <button className="border-2 px-5 py-3 rounded-[20px] bg-green-500" type="submit" disabled={isLoading}>{isLoading ? "Submitting..." : "Submit"}</button>
+              </div>
+            </form>
+          </div>
+
+          <div className="my-3 p-5 flex flex-col w-full rounded-[25px] bg-white lg:w-1/2 lg:p-10 lg:ml-3 ">
+            <div className=" text-2xl w-full h-fit  text-right ">
+
+              <div className="flex flex-row justify-end lg:text-2xl">
+                <div className="pr-5">
+                  <span className="underline underline-offset-4">Adress:</span>
                 </div>
-                <div className="mb-5 border-b-2 border-solid border-black">
-                  <input className=" w-full p-2" type="text" name='fullname' id='fullname' value={form.fullname} onChange={handleChange} required/>
+                <div>
+                  <span>Jihlavská 895, Havlíčkův Brod, 580 01</span>
                 </div>
-                <div className="text-2xl my-5">
-                  <label htmlFor="email">Email</label>
-                </div>
-                <div className="mb-5 border-b-2 border-solid border-black">
-                  <input className="w-full p-2" type="email" name="email" id='email' value={form.email} onChange={handleChange} required/>
-                </div>
-                <div className="text-2xl my-5">
-                  <label htmlFor="phone">Phone</label>
-                </div>
-                <div className="mb-5 border-b-2 border-solid border-black">
-                  <input className=" w-full p-2" type="text" name='phone' id='phone' value={form.phone} onChange={handleChange} required/>
-                </div> 
-                <div className="text-2xl my-5">
-                  <label htmlFor="msg">Message</label>
-                </div>
-                <div className="mb-5 border-b-2 border-solid border-black">
-                  <textarea className=" w-full p-2"name='msg' id='msg' value={form.msg} onChange={handleChange} required></textarea>
-                </div>
-                <div className="text-xl font-semibold ">
-                  <button className="border-2 px-5 py-3 rounded-[20px] bg-green-500" type="submit" disabled={isLoading}>{isLoading ? "Submitting..." : "Submit"}</button>
-                </div>
-                </form>
               </div>
 
-              <div className="my-3 p-5 flex flex-col w-full rounded-[25px] bg-white lg:w-1/2 lg:p-10 lg:ml-3 ">  
-                <div className=" text-2xl w-full h-fit  text-right ">
-
-                  <div className="flex flex-row justify-end lg:text-2xl">
-                    <div className="pr-5">
-                      <span className="underline underline-offset-4">Adress:</span>
-                    </div>
-                    <div>
-                      <span>Jihlavská 895, Havlíčkův Brod, 580 01</span>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-row my-3 justify-end">
-                    <div className="pr-5">
-                      <span className="underline underline-offset-4">Reception:</span>
-                    </div>
-                    <div>
-                      <span>+420 606 838 786</span>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-row flex-wrap justify-end">
-                    <div className="pr-5">
-                      <span className="underline underline-offset-4">E-mail:</span>
-                    </div>
-                    <div>
-                      <span >arena<br/>@arenaapartmentshb.cz</span>
-                    </div>
-                  </div>
+              <div className="flex flex-row my-3 justify-end">
+                <div className="pr-5">
+                  <span className="underline underline-offset-4">Reception:</span>
                 </div>
-                {/*Mapa*/}
-                <div className="py-4 h-96">
-                <Canvas frameloop="demand" orthographic camera={{ position: [0, 0, 50], zoom: 2, up: [0, 0, 1], far: 1000 }} className='rounded-[20px]'>
-                <Sphere args={[8]} material-color="green" position={[21,104,1]} onClick={(event) =>
-                  showToast("Arena Apartments", "Jihlavská 895", "606 838 786")}/>
-                <Sphere args={[8]} material-color="brown" position={[87,-266,1]} onClick={(event) =>
-                  showToast("Restaurant na Ostrově", "Žižkova 3329", "720 071 973")}/>
-                <Sphere args={[8]} material-color="black" position={[105,-259,1]} onClick={(event) =>
-                  showToast("House of Culture Ostrov", "Na Ostrově 28/3", "702 200 208")}/>
-                <Sphere args={[8]} material-color="blue" position={[123,-253,1]} onClick={(event) =>
-                  showToast("Bar on the Ostrově", "Žižkova 3329", "728 507 947")}/>
+                <div>
+                  <span>+420 606 838 786</span>
+                </div>
+              </div>
+
+              <div className="flex flex-row flex-wrap justify-end">
+                <div className="pr-5">
+                  <span className="underline underline-offset-4">E-mail:</span>
+                </div>
+                <div>
+                  <span >arena<br />@arenaapartmentshb.cz</span>
+                </div>
+              </div>
+            </div>
+            {/*Mapa*/}
+            <div className="py-4 h-96">
+              <Canvas frameloop="demand" orthographic camera={{ position: [0, 0, 50], zoom: 2, up: [0, 0, 1], far: 1000 }} className='rounded-[20px]'>
+                <Sphere args={[8]} material-color="green" position={[21, 104, 1]} onClick={(event) =>
+                  showToast("Arena Apartments", "Jihlavská 895", "606 838 786")} />
+                <Sphere args={[8]} material-color="brown" position={[87, -266, 1]} onClick={(event) =>
+                  showToast("Restaurant na Ostrově", "Žižkova 3329", "720 071 973")} />
+                <Sphere args={[8]} material-color="black" position={[105, -259, 1]} onClick={(event) =>
+                  showToast("House of Culture Ostrov", "Na Ostrově 28/3", "702 200 208")} />
+                <Sphere args={[8]} material-color="blue" position={[123, -253, 1]} onClick={(event) =>
+                  showToast("Bar on the Ostrově", "Žižkova 3329", "728 507 947")} />
                 <Suspense fallback={null}>
                   <Svg url="/arenaMap.svg" />
-                  </Suspense>
-                  <MapControls enableRotate={false}/>
-                </Canvas>
-                </div>
-              </div>
-
+                </Suspense>
+                <MapControls enableRotate={false} />
+              </Canvas>
             </div>
           </div>
-          <ToastContainer/>
-          </motion.div>
-    );
+
+        </div>
+      </div>
+      <ToastContainer />
+    </motion.div>
+  );
 };
